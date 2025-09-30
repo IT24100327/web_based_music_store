72e2373040b8f37e4ceec2d9b98bfe2012be2978
IT24100327 <it24100327@my.sliit.lk>
2025-09-30 14:17:46 +0530

    Added Email Util

---
+++ src/main/java/utils/EmailHandleUtil.java
@@ -0,0 +1,40 @@
+package utils;
+import jakarta.mail.*;
+import jakarta.mail.Message;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+import java.util.Properties;
+
+public class EmailHandleUtil {
+    public static void sendMail(String to, String subject, String body) throws Exception {
+        final String username = "prageethperera2002@gmail.com";   // Your Gmail
+        final String password = "bgma yxkv cvsq glzw";    // 16-char app password
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587"); // TLS port
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(username));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+        message.setSubject(subject);
+
+        message.setContent(body, "text/html; charset=utf-8");
+
+        Transport.send(message);
+        System.out.println("Email sent successfully to " + to);
+    }
+
+}
